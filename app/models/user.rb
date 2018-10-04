93cc0bc35f71ea6d1a8dd0ec6c9df7c0e29d53ab
AkihoKurata <43805111+AkihoKurata@users.noreply.github.com>
2018-10-04 16:33:11 +0800

    Signup function (#3)
    * Signup-function
    
    * Fixed Spell confimation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
+
+	before_save { email.downcase! }
+	EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+	validates :name, presence: true, length: { minimum: 3, maximum: 30 }
+	validates :email, presence: true, length: { minimum: 5, maximum: 50 }, format: { with: EMAIL_REGEX }, uniqueness: { case_sensitive: false}
+
 	has_secure_password
 end
