c1ba908d63817d67a4782d2483c7982144fafacc
倉田あき穗 <akiho@kurataakihonoMacBook-puro.local>
2018-10-15 14:26:54 +0800

    Fixed miss spelled

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,10 @@
+<<<<<<< Updated upstream
 class CategoriesController < ApplicationControlle
 
+=======
+class CategoriesController < ApplicationController
+	def index
+		
+	end
+>>>>>>> Stashed changes
 end
