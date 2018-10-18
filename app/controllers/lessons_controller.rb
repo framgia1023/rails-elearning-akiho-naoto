a59dc3a29a562c4d50064576e0c2a40689798698
倉田あき穗 <akiho@kurataakihonoMacBook-puro.local>
2018-10-18 17:38:35 +0800

    fixed redundancy

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -12,9 +12,9 @@ class LessonsController < ApplicationController
 	def show
 		@lesson = Lesson.find(params[:id])
 		choices = @lesson.answers.collect{ |a| a.choice_id }
-		score = Choice.find(choices).collect{ |c| c.correct }.count(true)
+		@score = Choice.find(choices).collect{ |c| c.correct }.count(true)
 		if @lesson.result == nil
-			@lesson.update(result: score)
+			@lesson.update(result: @score)
 		end
 		# @words = answers.collect{ |a| a.word_id }
 		
--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -1,7 +1,3 @@
 module LessonsHelper
 
-	def result(lesson)
-		choices = lesson.answers.collect{ |a| a.choice_id }
-		Choice.find(choices).collect{ |c| c.correct }.count(true)	
-	end
 end
