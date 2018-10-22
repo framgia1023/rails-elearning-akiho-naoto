171c21df14e57c570e95d383dbcae05a6f78155c
Akiho Kurata <akiho.kurata922@gmail.com>
2018-10-22 15:44:13 +0800

    Fixed learned word page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
 	def home
 		if logged_in?
 			@activities = current_user.feed.paginate(page: params[:page], per_page: 5).order(created_at: :desc)
+			@answers = Answer.where(lesson_id: current_user.lessons.ids)
 			# answers = Answer.where(lesson_id: current_user.lessons.ids)
 			# @learned_words = answers.collect{ |a| a.choice.correct }.count(true)
 		end		
