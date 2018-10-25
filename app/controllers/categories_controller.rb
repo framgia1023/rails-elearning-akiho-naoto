6da4259a17a9ff7250ef4036740c697867f449f2
Akiho Kurata <akiho.kurata922@gmail.com>
2018-10-25 15:57:48 +0800

    Fixed error for no word category and filter categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,9 +2,16 @@
 class CategoriesController < ApplicationController
 
 	def index
-		@categories = Category.paginate(page: params[:page], per_page: 6)
 		@lesson = Lesson.new
 		@learned_category = Lesson.all.collect{ |l| l.category_id if l.user_id == current_user.id }.compact
+		if params[:status] == "all" || params[:status] == nil
+			@categories = Category.paginate(page: params[:page], per_page: 6)
+		elsif params[:status] == "learned"
+			@categories = Category.where(id: @learned_category).paginate(page: params[:page], per_page: 6)
+		elsif params[:status] == "unlearned"
+			@categories = Category.where.not(id: @learned_category).paginate(page: params[:page], per_page: 6)
+		end
+
 	end
 
 end
