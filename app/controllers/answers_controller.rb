3fbdd44a6039030fe1c3b391c28997ab4cfbabcd
Akiho Kurata <akiho.kurata922@gmail.com>
2018-10-26 17:46:02 +0800

    Fixed Lesson and remove footer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,5 @@
 class AnswersController < ApplicationController
+	# before_action :check_answered, only: :create
 
 	def new
 		@lesson = Lesson.find(params[:lesson_id])
@@ -12,16 +13,32 @@ class AnswersController < ApplicationController
 	def create
 		@answer = Answer.new(answer_params)
 		@lesson = Lesson.find(params[:lesson_id])
-		if @answer.save && !(@lesson.next_word == nil)
+		answer = Answer.where(lesson_id: params[:lesson_id], word_id: params[:word_id])
+		if !answer.present?
+			if @answer.save && !@lesson.next_word.nil?
+				redirect_to new_lesson_answer_url
+			else
+				redirect_to lesson_url(@lesson)
+			end
+			
+		elsif @lesson.result.nil?
 			redirect_to new_lesson_answer_url
-		else
+
+		elsif @lesson.result.present?
 			redirect_to lesson_url(@lesson)
 		end
-
 	end
 
 	private
 	def answer_params
 		params.permit(:lesson_id, :word_id, :choice_id)
 	end
+
+	# def check_answered
+	# 	@lesson = Lesson.find(params[:lesson_id])
+	# 	answer = Answer.where(lesson_id: params[:lesson_id], word_id: params[:word_id])
+	# 	if !answer.nil?
+	# 		render = "new"
+	# 	end		
+	# end
 end
